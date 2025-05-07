b810ab527c39425e6f2c31acac526d6f12a48d92
Guen Nhat King <guennhatking@gmail.com>
2025-05-07 11:03:49 +0700

    Cập nhật hồ sơ

--- ShoppeWebApp/Areas/Customer/Controllers/ProfilesController.cs
+++ ShoppeWebApp/Areas/Customer/Controllers/ProfilesController.cs
@@ -55,54 +55,43 @@ namespace ShoppeWebApp.Areas.Customer.Controllers
                 HoVaTen = user.HoVaTen,
                 SoDienThoai = user.Sdt,
                 UrlAnhDaiDien = user.UrlAnh,
+                Cccd = user.Cccd,
+                DiaChi = user.DiaChi,
+                SoDu = user.SoDu,
             };
             return View(profiles);
         }
-        //[HttpPost]
-        //public async Task<IActionResult> ChangeProfiles(string? username, int? id, Profiles profiles, IFormFile? AnhDaiDien = null)
-        //{
-        //    if (username == null || id == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    int? currUserId = GetUserId();
-        //    string? currUsername = HttpContext.User.Identity?.Name;
-        //    if (currUserId == null || currUsername == null || username != currUsername || id != currUserId)
-        //    {
-        //        return RedirectToAction("AccessDenied", "Authentication", new { area = "" });
-        //    }
-        //    var user = await _context.NguoiDungs.FirstOrDefaultAsync(i => i.MaNguoiDung == id);
-        //    if (user == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    if (ModelState.IsValid)
-        //    {
-        //        try
-        //        {
-        //            user.HoVaTen = profiles.HoVaTen;
-        //            user.NgaySinh = profiles.NgaySinh;
-        //            user.Email = profiles.Email;
-        //            user.SoDienThoai = profiles.SoDienThoai;
-        //            _context.Update(user);
-        //            await _context.SaveChangesAsync();
-        //            var imageUpload = new ImageUpload(_webHost);
-        //            if (await imageUpload.SaveImageAs(AnhDaiDien!))
-        //            {
-        //                imageUpload.DeleteImage(user.UrlAnhDaiDien!);
-        //                user.UrlAnhDaiDien = imageUpload.FileName;
-        //            }
-        //            _context.Update(user);
-        //            await _context.SaveChangesAsync();
-        //        }
-        //        catch (DbUpdateConcurrencyException ex)
-        //        {
-        //            return BadRequest(ex.Message);
-        //        }
-        //        return RedirectToAction("Index", new { username = username, id = id });
-        //    }
-        //    return View(profiles);
-        //}
+        [HttpPost]
+        public async Task<IActionResult> ChangeProfiles(Profiles profiles, IFormFile? AnhDaiDien = null)
+        {
+            string? currUserId = GetUserId();
+            var user = await _context.Nguoidungs.FirstOrDefaultAsync(i => i.IdNguoiDung == currUserId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    user.HoVaTen = profiles.HoVaTen;
+                    user.Sdt = profiles.SoDienThoai;
+                    user.Email = profiles.Email;
+                    user.DiaChi = profiles.DiaChi;
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
+                    // upload image here                        
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+                return RedirectToAction("Index");
+            }
+            Console.WriteLine("Invalid!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            return View(profiles);
+        }
         private string? GetUserId()
         {
             return HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value.ToString();
--- ShoppeWebApp/ViewModels/Customer/Profiles.cs
+++ ShoppeWebApp/ViewModels/Customer/Profiles.cs
@@ -1,14 +1,20 @@
-﻿namespace ShoppeWebApp.ViewModels.Customer
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace ShoppeWebApp.ViewModels.Customer
 {
     public class Profiles
     {
-        public string IdNguoiDung { get; set; }
+        [ValidateNever]
+        public string IdNguoiDung { get; set; } = null!;
+        [ValidateNever]
         public string? UrlAnhDaiDien { get; set; }
         public string HoVaTen { get; set; } = null!;
+        [ValidateNever]
         public string Cccd { get; set; } = null!;
         public string SoDienThoai { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string DiaChi { get; set; } = null!;
+        [ValidateNever]
         public decimal SoDu { get; set; }
     }
 }
