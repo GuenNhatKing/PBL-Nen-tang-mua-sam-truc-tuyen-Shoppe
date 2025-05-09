034abcfb90168a3ce28bca92a9f431ba61c83e72
Guen Nhat King <guennhatking@gmail.com>
2025-05-09 15:46:10 +0700

    Quản lí đơn hàng

--- ShoppeWebApp/ViewModels/Customer/ManageProducts.cs
+++ ShoppeWebApp/ViewModels/Customer/ManageProducts.cs
@@ -9,11 +9,40 @@ namespace ShoppeWebApp.ViewModels.Customer
         [ValidateNever]
         public string? UrlAnhDaiDien { get; set; }
         public string HoVaTen { get; set; } = null!;
-        public string Cccd { get; set; } = null!;
-        public string SoDienThoai { get; set; } = null!;
         public string Email { get; set; } = null!;
-        public string DiaChi { get; set; } = null!;
-        [ValidateNever]
-        public decimal SoDu { get; set; }
+        public List<ManageProductOrder> danhSachDonHang = new List<ManageProductOrder>();
+    }
+    public class ManageProductOrder
+    {
+        public string IdDonHang { get; set; } = null!;
+        public int TinhTrang { get; set; }
+        public DateTime? ThoiGianTao { get; set; }
+        public DateTime? ThoiGianGiao { get; set; }
+        public List<OrderDescCuaHang> danhSachCuaHang = new List<OrderDescCuaHang>();
+        public decimal TongTien { get; set; }
+    }
+    public class OrderDescCuaHang
+    {
+        public string IdCuaHang { get; set; } = null!;
+        public string TenCuaHang { get; set; } = null!;
+        public List<OrderDescSanPham> danhSachSanPhams = new List<OrderDescSanPham>();
+    }
+    public class OrderDescSanPham
+    {
+        public string IdSanPham { get; set; } = null!;
+        public string TenSanPham { get; set; } = null!;
+        public string UrlAnhSanPham { get; set; } = null!;
+        public decimal DonGia { get; set; }
+        public int SoLuong { get; set; }
+        
+        public List<DanhGiaInfo> danhGias = new List<DanhGiaInfo>();
+    }
+
+    public class DanhGiaInfo
+    {
+        public string IdDanhGia { get; set; } = null!;
+        public string? NoiDungDanhGia { get; set; } = null!;
+        public DateTime? ThoiGianDanhGia { get; set; }
+        public int SoSaoDanhGia { get; set; }
     }
 }
